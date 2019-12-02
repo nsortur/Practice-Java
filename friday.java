715a7a4a086fe827f4bbf4685368ea95aade2a20
nsortur <sortur.neel@charter.local>
2019-12-02 10:15:00 -0500

    friday progress

--- friday.java
+++ friday.java
@@ -17,46 +17,76 @@ public class friday {
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("gift1.out")));
 
 		ArrayList<Integer> frequencies = new ArrayList<Integer>();
-		
-		for(int i = 0; i < 7; i++) {
+
+		for (int i = 0; i < 7; i++) {
 			frequencies.add(0);
 		}
-	
-		
+
+//		int leftOver = days[i] % 7;
+
 		int years = scan.nextInt();
 		int leapYears = getLeapYears(years);
-		
-		int totalDays = ((years-leapYears) * 365) + (leapYears * 366);
-		System.out.println("total days: " + totalDays);
-		
-		int[]days = {31,28,31,30,31,30,31,31,30,31,30,31};
-		int[]leapDays = {31,29,31,30,31,30,31,31,30,31,30,31};
 
-		for(int i=0; i < years * 12; i++) {
-			int leftOver = days[i] % 7;
-			
+		int totalDays = ((years - leapYears) * 365) + (leapYears * 366);
+		System.out.println("total days: " + totalDays);
+
+		int weekDay = 0;
+		int[] days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		int[] leapDays = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+		for (int i = 0; i < years; i++) {
+			// Each year
+
+			for (int j = 0; j < days.length; j++) {
+				// Each month
+				for (int k = 0; k < days[j]; k++) {
+					// Each day
+
+					weekDay++;
+					if (weekDay > 6) {
+						weekDay = 0;
+					}
+				}
+			}
 		}
-		
-		for(int i = 0; i < frequencies.size(); i++) {
+
+		for (int i = 0; i < frequencies.size(); i++) {
 			System.out.print(frequencies.get(i) + " ");
 		}
 	}
-	public static int getLeapYears(int years){
+
+	public static int getLeapYears(int years) {
 		int numLeapYears = 0;
-		
-		for(int i=1900; i <= 1900 + years; i++) {
-			if(i % 100 == 0) {
-				if(i % 400 == 0) {
+
+		for (int i = 1900; i <= 1900 + years; i++) {
+			if (i % 100 == 0) {
+				if (i % 400 == 0) {
 					numLeapYears++;
 				}
 			} else {
-				if(i % 4 == 0) {
+				if (i % 4 == 0) {
 					numLeapYears++;
 				}
 			}
 		}
-		
+
 		return numLeapYears;
+	}
+
+	public static boolean isLeapYear(int input) {
+		int year = 1900 + input;
+		boolean returnThis = false;
+		
+		if (year % 100 == 0) {
+			if(year % 400 == 0) {
+				returnThis = true;
+			}
+		} else {
+			if (year % 4 == 0) {
+				returnThis = true;
+			}
+		}
+		return returnThis;
 	}
 //	for(int i=0; i <= totalDays; i++) {
 //		if(i % 13 == 0) {
