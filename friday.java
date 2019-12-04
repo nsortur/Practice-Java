2fb9c7b52fa590740dbf50ddd0b91142a9d6157f
Neel Sortur <nsortur@Neels-MacBook-Pro.local>
2019-12-03 22:12:30 -0500

    completed friday

--- friday.java
+++ friday.java
@@ -13,8 +13,8 @@ TASK: friday
 */
 public class friday {
 	public static void main(String[] args) throws Exception {
-		Scanner scan = new Scanner(new BufferedReader(new FileReader("friday.txt")));
-		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("gift1.out")));
+		Scanner scan = new Scanner(new BufferedReader(new FileReader("friday.in")));
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("friday.out")));
 
 		ArrayList<Integer> frequencies = new ArrayList<Integer>();
 
@@ -22,13 +22,7 @@ public class friday {
 			frequencies.add(0);
 		}
 
-//		int leftOver = days[i] % 7;
-
 		int years = scan.nextInt();
-		int leapYears = getLeapYears(years);
-
-		int totalDays = ((years - leapYears) * 365) + (leapYears * 366);
-		System.out.println("total days: " + totalDays);
 
 		int weekDay = 0;
 		int[] days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
@@ -36,12 +30,17 @@ public class friday {
 
 		for (int i = 0; i < years; i++) {
 			// Each year
+			int tempMonths = (isLeapYear(i)) ? leapDays.length : days.length;
 
-			for (int j = 0; j < days.length; j++) {
+			for (int j = 0; j < tempMonths; j++) {
 				// Each month
-				for (int k = 0; k < days[j]; k++) {
-					// Each day
 
+				int tempDays = (isLeapYear(i)) ? leapDays[j] : days[j];
+				for (int k = 0; k < tempDays; k++) {
+					// Each day
+					if (k == 13) {
+						frequencies.set(weekDay, frequencies.get(weekDay) + 1);
+					}
 					weekDay++;
 					if (weekDay > 6) {
 						weekDay = 0;
@@ -49,36 +48,27 @@ public class friday {
 				}
 			}
 		}
+		frequencies.add(0, frequencies.get(6));
+		frequencies.remove(7);
 
 		for (int i = 0; i < frequencies.size(); i++) {
-			System.out.print(frequencies.get(i) + " ");
-		}
-	}
+			if (i == 6) {
+				out.print(frequencies.get(i));
+				out.println();
 
-	public static int getLeapYears(int years) {
-		int numLeapYears = 0;
-
-		for (int i = 1900; i <= 1900 + years; i++) {
-			if (i % 100 == 0) {
-				if (i % 400 == 0) {
-					numLeapYears++;
-				}
 			} else {
-				if (i % 4 == 0) {
-					numLeapYears++;
-				}
+				out.print(frequencies.get(i) + " ");
 			}
 		}
-
-		return numLeapYears;
+		out.close();
 	}
 
 	public static boolean isLeapYear(int input) {
 		int year = 1900 + input;
 		boolean returnThis = false;
-		
+
 		if (year % 100 == 0) {
-			if(year % 400 == 0) {
+			if (year % 400 == 0) {
 				returnThis = true;
 			}
 		} else {
@@ -88,25 +78,21 @@ public class friday {
 		}
 		return returnThis;
 	}
-//	for(int i=0; i <= totalDays; i++) {
-//		if(i % 13 == 0) {
-//			if(i % 7 == 1) {
-//				frequencies.set(0, frequencies.get(0) + 1);
-//			} else if(i % 7 == 2) {
-//				frequencies.set(1, frequencies.get(1) + 1);
-//			} else if(i % 7 == 3) {
-//				frequencies.set(2, frequencies.get(2) + 1);
-//			} else if(i % 7 == 4) {
-//				frequencies.set(3, frequencies.get(3) + 1);
-//			} else if(i % 7 == 5) {
-//				frequencies.set(4, frequencies.get(4) + 1);
-//			} else if(i % 7 == 6) {
-//				frequencies.set(5, frequencies.get(5) + 1);
-//			} else if(i % 7 == 0) {
-//				frequencies.set(6, frequencies.get(6) + 1);
+//	public static int getLeapYears(int years) {
+//		int numLeapYears = 0;
+//
+//		for (int i = 1900; i <= 1900 + years; i++) {
+//			if (i % 100 == 0) {
+//				if (i % 400 == 0) {
+//					numLeapYears++;
+//				}
 //			} else {
-//				System.out.println("Wat");
+//				if (i % 4 == 0) {
+//					numLeapYears++;
+//				}
 //			}
 //		}
+//
+//		return numLeapYears;
 //	}
 }
